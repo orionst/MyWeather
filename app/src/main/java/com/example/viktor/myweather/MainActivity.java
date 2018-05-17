a63a6b4fa409d49b98c72dc28a36bda6986d67a8
Viktor <vm.orion@gmail.com>
2018-05-17 03:11:57 +0300

    HW 2: На activity добавлен текст и изображение. Программу LifeCycles интегрировал в данном проекте. Добавил вывод в logcat. (вывод событий жизненного цикла не стал убирать из хардкода, в следующих ДЗ совсем удалю этот кусок)

--- app/src/main/java/com/example/viktor/myweather/MainActivity.java
+++ app/src/main/java/com/example/viktor/myweather/MainActivity.java
@@ -1,13 +1,88 @@
 package com.example.viktor.myweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "weatherLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String instanceState;
+        if (savedInstanceState == null){
+            instanceState = "Первый запуск!";
+        }
+        else{
+            instanceState = "Повторный запуск!";
+        }
+        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, instanceState + " - onCreate()");
+
+        TextView date = (TextView) findViewById(R.id.dateTitle);
+        date.setText(new WeatherConditionBuilder(getResources()).getTodayDate());
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(getApplicationContext(), "onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onRestoreInstanceState: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onSaveInstanceState: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onRestart: ");
+    }
+
 }
---
+++ app/src/main/java/com/example/viktor/myweather/WeatherConditionBuilder.java
@@ -0,0 +1,20 @@
+package com.example.viktor.myweather;
+
+import android.content.res.Resources;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class WeatherConditionBuilder {
+
+    Resources resources;
+
+    public WeatherConditionBuilder(Resources resources) {
+        this.resources = resources;
+    }
+
+    public String getTodayDate() {
+        String dateTitle = resources.getString(R.string.date_title);
+        return String.format("%s %s", dateTitle, new SimpleDateFormat("dd.MM.yyyy HH:mm").format(Calendar.getInstance().getTime()));
+    }
+}
