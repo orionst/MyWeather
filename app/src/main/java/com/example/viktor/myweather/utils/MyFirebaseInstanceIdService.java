6fd039d49179869b5fcb46d878766fa39b4c3b27
Viktor <vm.orion@gmail.com>
2018-07-09 15:01:54 +0300

    HW 7: Добавлены классы для работы с push уведомлениями. При получении уведомления выводится notification и по нажатию по нему должна открываться активити AlertActivity с заполнением из данных пуша.

---
+++ app/src/main/java/com/example/viktor/myweather/utils/MyFirebaseInstanceIdService.java
@@ -0,0 +1,28 @@
+package com.example.viktor.myweather.utils;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Получить ключ установки приложения на устройство
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        String TAG = "PushIDService";
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    // Метод отправки ключа в вашу БД
+    private void sendRegistrationToServer(String refreshedToken){
+    }
+
+
+}
